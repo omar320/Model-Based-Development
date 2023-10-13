34152182d77ecdd76755ba8bd77094b9be689409
omar320 <69600283+omar320@users.noreply.github.com>
2023-10-13 21:50:35 +0300

    Add files via upload

---
+++ MATLAB_BASIC/Lab_3/Basic_Plotting.m
@@ -0,0 +1,6 @@
+x=[0:0.1:2*pi];
+y=sin(x);
+plot(x,y,'r*:','LineWidth',2);
+xlabel('x');
+ylabel('y');
+legend('Plot for sine wave');
---
+++ MATLAB_BASIC/Lab_3/Structures.m
@@ -0,0 +1,4 @@
+Person1 = struct('Name','Omar','Age',22,'City','El Salam');
+disp(Person1.Name);
+disp(Person1.Age);
+disp(Person1.City);
